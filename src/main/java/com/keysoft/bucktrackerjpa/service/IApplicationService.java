422dc1ce3709246542eae228f925a933e25a4084
Alex Frost <zufrost.a@gmail.com>
2020-06-03 17:37:47 +0300

    Updating objects

---
+++ src/main/java/com/keysoft/bucktrackerjpa/service/IApplicationService.java
@@ -0,0 +1,11 @@
+package com.keysoft.bucktrackerjpa.service;
+
+import com.keysoft.bucktrackerjpa.entity.Application;
+
+import java.util.List;
+
+public interface IApplicationService {
+    boolean addApplication(Application application);
+    Application getApplicationById(int applicationId);
+    void updateApplication(Application application);
+}
